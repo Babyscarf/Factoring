5f5a095df74f92af5e1827cf1620d30d1bd3f6a8
Kimo <kimorahmani@gmail.com>
2017-10-31 11:48:59 +0100

    Labb Factoring

---
+++ src/QuadraticSieve.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+public class QuadraticSieve {
+
+    public static void main(String[] args) {
+	
+	new QuadraticSieve();
+}
+
+public QuadraticSieve(){
+	alg();
+}
+
+private void alg() {
+	System.out.println("test");
+	double bSmooth=2.0;
+	Math.abs(2);
+	}
+}
