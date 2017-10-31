59e31b9edc56a5c4e018ed3069cc9eeb441d2bc0
Kimo <kimorahmani@gmail.com>
2017-10-31 11:51:38 +0100

    addade Trial algoritmen

--- src/QuadraticSieve.java
+++ src/QuadraticSieve.java
@@ -1,4 +1,5 @@
 import java.lang.Math;
+import java.util.ArrayList;
 public class QuadraticSieve {
 
     public static void main(String[] args) {
@@ -13,6 +14,30 @@ public QuadraticSieve(){
 private void alg() {
 	System.out.println("test");
 	double bSmooth=2.0;
-	Math.abs(2);
+	
 	}
+public ArrayList<Integer> trial_division(int n){
+
+    ArrayList<Integer> prime= new ArrayList<>();
+    if(n<2) {
+        prime.add(n);
+        return prime;
+    }
+    int loop=(int)Math.sqrt(n) ;
+    System.out.println(loop);
+    for(int i=2; i<=loop; i++) {
+        if (i * i > n)
+            break;
+        while(n % i==0){
+
+            prime.add(i);
+            n/=i;
+        }
+    }
+    if(n>1)
+        prime.add(n);
+
+    System.out.println(prime.toString());
+    return prime;
+}
 }
