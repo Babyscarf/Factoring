ff557b446d634c2695e369db492211cdf65e27ec
Kimo <kimorahmani@gmail.com>
2017-11-02 12:41:20 +0100

    added comments and fixed the structure

--- src/QuadraticSieve.java
+++ src/QuadraticSieve.java
@@ -1,14 +1,14 @@
 import java.lang.Math;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class QuadraticSieve {
     static ArrayList<Integer> prim = new ArrayList<>();
     public static void main(String[] args) {
 
         QuadraticSieve test = new QuadraticSieve();
-        int bSmooth = test.alg();
-        test.isPrime(bSmooth);
+        int bSmooth = test. b_Smooth();
+        test.SieveOfEratosthenes(bSmooth);
         for(int i=1; i<prim.size(); i++){
 
            int value= test.Jacobi(87463,prim.get(i));
@@ -24,9 +24,47 @@ public class QuadraticSieve {
 
     }
 
+    //Find factors when N is small
+    public ArrayList<Integer> trial_division(int n) {
+
+        ArrayList<Integer> prime = new ArrayList<>();
+        if (n < 2) {
+            prime.add(n);
+            return prime;
+        }
+        int loop = (int) Math.sqrt(n);
+        System.out.println(loop);
+        for (int i = 2; i <= 52; i++) {
+            if (i * i > n)
+                break;
+            while (n % i == 0) {
+                // System.out.println("i:" + i);
+                prime.add(i);
+                n /= i;
+            }
+        }
+        if (n > 1)
+            prime.add(n);
+
+        System.out.println(prime.toString());
+        return prime;
+    }
 
 
-    private void isPrime(int bSmooth) {
+    // Find b-Smooth
+    private int b_Smooth() {
+        double input = 100000010;
+        //  System.out.println("test");
+        double bSmooth = Math.exp(Math.sqrt(Math.log(input) * Math.log(Math.log(input))) * 0.5);
+        System.out.println("b: " + bSmooth);
+        //if(bSmooth%)
+        // System.out.println("mod: " + 47%47);
+        return (int) bSmooth;
+
+
+    }
+        // Find all primes for p<bSmooth and store in the ArrayList
+    private void SieveOfEratosthenes(int bSmooth) {
         boolean[] prime = new boolean[bSmooth + 1];
 
         System.out.println(prime.length);
@@ -50,60 +88,39 @@ public class QuadraticSieve {
             System.out.println(prim.toString());
         }
 
-//    Input: an integer n > 1.
-//
-//    Let A be an array of Boolean values, indexed by integers 2 to n,
-//    initially all set to true.
-//
-//            for i = 2, 3, 4, ..., not exceeding √n:
-//            if A[i] is true:
-//            for j = i2, i2+i, i2+2i, i2+3i, ..., not exceeding n:
-//    A[j] := false.
-//
-//    Output: all i such that A[i] is true.
+    /*Determine if a number a is a quadratic residue to the modulus p, a^(p-1/2). If output
+      is 1, then there is a perfect square such that there exist a x so x^2=a mod p.
+      If output is -1 there is NO perfect square.
+      Output 0 means that a|p. However, we'll ignore that output.
+      We'll save all p that gives the output 1 and erase from the ArrayList given from
+      SieveOfEratosthenes method otherwise*/
 
-    public QuadraticSieve() {
-            //alg();
-        }
-//    Jacobi(a,n) {
-//        j := 1
-//        while (a not 0) do {
-//            while (a even) do {
-//                a := a/2
-//                if (n = 3 (mod 8) or n = 5 (mod 8)) then j := -j
-//            }
-//            interchange(a,n)
-//            if (a = 3 (mod 4) and n = 3 (mod 4)) then j := -j
-//            a := a mod n
-//        }
-//        if (n = 1) then return (j) else return(0)
-//    }
- public int Jacobi(int a, int n)
+    public int Jacobi(int a, int n)
 /* Precondition: a, n >= 0; n is odd */
-{
-    int ans=0;
-    if (a == 0)
-        ans = (n==1) ? 1 : 0;
-    else if (a == 2) {
-        switch (n%8) {
-            case 1:
-            case 7:
-                ans = 1;
-                break;
-            case 3:
-            case 5:
-                ans = -1;
-                break;
+    {
+        int ans=0;
+        if (a == 0)
+            ans = (n==1) ? 1 : 0;
+        else if (a == 2) {
+            switch (n%8) {
+                case 1:
+                case 7:
+                    ans = 1;
+                    break;
+                case 3:
+                case 5:
+                    ans = -1;
+                    break;
+            }
         }
+        else if ( a >= n )
+            ans = Jacobi(a%n, n);
+        else if (a%2 == 0)
+            ans = Jacobi(2,n)*Jacobi(a/2, n);
+        else
+            ans = (a%4 == 3 && n%4 == 3) ? -Jacobi(n,a) : Jacobi(n,a);
+        return ans;
     }
-    else if ( a >= n )
-        ans = Jacobi(a%n, n);
-    else if (a%2 == 0)
-        ans = Jacobi(2,n)*Jacobi(a/2, n);
-    else
-        ans = (a%4 == 3 && n%4 == 3) ? -Jacobi(n,a) : Jacobi(n,a);
-    return ans;
-}
 //    public int Jacobi(int a,int b) {
 //        if(b<=0 || (b % 2)==0)
 //            return 0;
@@ -143,41 +160,24 @@ public class QuadraticSieve {
 //         else
 //             return 0;
 //    }
+//    Input: an integer n > 1.
+//
+//    Let A be an array of Boolean values, indexed by integers 2 to n,
+//    initially all set to true.
+//
+//            for i = 2, 3, 4, ..., not exceeding √n:
+//            if A[i] is true:
+//            for j = i2, i2+i, i2+2i, i2+3i, ..., not exceeding n:
+//    A[j] := false.
+//
+//    Output: all i such that A[i] is true.
 
-    private int alg() {
-        double input = 100000010;
-        //  System.out.println("test");
-        double bSmooth = Math.exp(Math.sqrt(Math.log(input) * Math.log(Math.log(input))) * 0.5);
-        System.out.println("b: " + bSmooth);
-        //if(bSmooth%)
-        // System.out.println("mod: " + 47%47);
-        return (int) bSmooth;
-
-
-    }
-
-    public ArrayList<Integer> trial_division(int n) {
-
-        ArrayList<Integer> prime = new ArrayList<>();
-        if (n < 2) {
-            prime.add(n);
-            return prime;
+    public QuadraticSieve() {
+            //alg();
         }
-        int loop = (int) Math.sqrt(n);
-        System.out.println(loop);
-        for (int i = 2; i <= 52; i++) {
-            if (i * i > n)
-                break;
-            while (n % i == 0) {
-                // System.out.println("i:" + i);
-                prime.add(i);
-                n /= i;
-            }
-        }
-        if (n > 1)
-            prime.add(n);
 
-        System.out.println(prime.toString());
-        return prime;
-    }
+
+
+
+
 }
